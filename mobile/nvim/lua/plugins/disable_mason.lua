f23c15bcf367910f24acb019371e25deb4979679
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-07-23 18:42:29 +0500

    feat: fixed lua_ls ruff_lsp autoinstall error

---
+++ mobile/nvim/lua/plugins/disable_mason.lua
@@ -0,0 +1,16 @@
+return 
+  {
+  "neovim/nvim-lspconfig",
+  opts = {
+      servers = {
+        ruff_lsp = {
+          mason = false,
+          enabled = false,
+        },
+        lua_ls = {
+          mason = false,
+          enabled = false,
+        },
+      },
+    },
+}
