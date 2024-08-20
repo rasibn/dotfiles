d6e3b91aa89a87068fe90936b18950e9adfe0efb
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-08-21 00:01:24 +0500

    chore: updated mobile nvim

---
+++ mobile/nvim/lua/plugins/oil.lua
@@ -0,0 +1,17 @@
+return {
+  "stevearc/oil.nvim",
+  lazy = false,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    { "<leader>fe", "<cmd>Oil<CR>", desc = "Oil file Explorer" },
+    { "<leader>e", "<cmd>Oil<CR><C-p>", desc = "Oil file Explorer (Preview)" },
+    { "<c-l>", false },
+    { "<C-h>", false },
+  },
+  opts = {
+    default_file_explorer = true,
+    view_options = {
+      show_hidden = true,
+    },
+  },
+}
