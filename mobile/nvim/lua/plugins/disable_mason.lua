0ee55ac9ac4123e5f49a648f5fbaf3b3ab8f386d
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-08-22 02:23:22 +0500

    updates

--- mobile/nvim/lua/plugins/disable_mason.lua
+++ mobile/nvim/lua/plugins/disable_mason.lua
@@ -1,16 +1,15 @@
-return 
-  {
+return {
   "neovim/nvim-lspconfig",
   opts = {
-      servers = {
-        ruff_lsp = {
-          mason = false,
-          enabled = false,
-        },
-        lua_ls = {
-          mason = false,
-          enabled = false,
-        },
+    servers = {
+      ruff_lsp = {
+        mason = false,
+        enabled = false,
+      },
+      lua_ls = {
+        mason = false,
+        enabled = false,
       },
     },
+  },
 }
