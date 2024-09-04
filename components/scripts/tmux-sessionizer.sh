89bd7552d13769a921693fe19b297f78d5f7ec35
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-09-05 01:34:02 +0500

    fix tmux-sessionizer.sh to use it as default for project picking

--- components/scripts/tmux-sessionizer.sh
+++ components/scripts/tmux-sessionizer.sh
@@ -8,12 +8,11 @@ command_exists() {
 if [[ $# -eq 1 ]]; then
   selected=$1
 else
-  # if command_exists fd; then
-  #   PREVIEW_CMD="fd --type f --max-depth 1 --color=always . {} | head -n 100"
-  # else
-  #   PREVIEW_CMD="find {} -maxdepth 1 -type f | head -n 100"
-  # fi
-  selected=$(find ~/Projects ~/work -mindepth 1 -maxdepth 1 -type d | fzf --preview "$PREVIEW_CMD")
+  if command_exists fd; then
+    selected=$(fd . --type d --max-depth 1 ~/Projects ~/work | fzf)
+  else
+    selected=$(find ~/Projects ~/work -mindepth 1 -maxdepth 1 -type d | fzf)
+  fi
 fi
 
 if [[ -z $selected ]]; then
@@ -24,12 +23,16 @@ selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-  tmux new-session -s "$selected_name" -c "$selected"
+  tmux new-session -s "$selected_name" -n "nvim" -c "$selected"
+  tmux new-window -t "$selected_name" -n "zsh" -c "$selected"
+  tmux new-window -t "$selected_name" -n "extra" -c "$selected"
   exit 0
 fi
 
 if ! tmux has-session -t="$selected_name" 2>/dev/null; then
-  tmux new-session -ds "$selected_name" -c "$selected"
+  tmux new-session -ds "$selected_name" -n "nvim" -c "$selected"
+  tmux new-window -t "$selected_name" -n "zsh" -c "$selected"
+  tmux new-window -t "$selected_name" -n "extra" -c "$selected"
 fi
 
 if [[ -z $TMUX ]]; then
