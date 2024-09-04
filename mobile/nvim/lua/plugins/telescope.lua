96bc64e0b9737c13d5c30910d94d33d15f60bd47
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-09-05 01:55:59 +0500

    updating mobile

--- mobile/nvim/lua/plugins/telescope.lua
+++ mobile/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
-return {}
--- return {
---   "nvim-telescope/telescope.nvim",
---   keys = { { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files (cwd)" } },
--- }
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = { { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files (cwd)" } },
+  -- keys = { { "<leader><space>", "<cmd>Telescope buffers<cr>", desc = "Switch Buffers" } },
+}
