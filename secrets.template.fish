6dc29ab19b73098cae63577e4656c6c066ec9b44
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-09-13 19:42:56 +0300

    fixing enviroment variable

--- secrets.template.fish
+++ secrets.template.fish
@@ -1,5 +1,6 @@
 set -x HOME_EMAIL ""
 set -x WORK_EMAIL ""
-set -x PROJECT_DIR $HOME/projects
-set -x WORK_DIR $HOME/work
-set -x DOTFILE_DIR $HOME/projects/dotfiles
+set -x PROJECT_DIR=$HOME/projects
+set -x WORK_DIR=$HOME/work
+set -x ASSET_DIR=$HOME/assets
+set -x DOTFILE_DIR=$ASSET_DIR/dotfiles
