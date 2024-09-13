ed8ed0e6e126bfc37365eeb951358358db8ce7ec
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-09-13 19:42:56 +0300

    fixing enviroment variable

--- secrets.template.sh
+++ secrets.template.sh
@@ -2,4 +2,5 @@ export HOME_EMAIL=
 export WORK_EMAIL=
 export PROJECT_DIR=$HOME/projects
 export WORK_DIR=$HOME/work
-export DOTFILE_DIR=$HOME/projects/dotfiles
+export ASSET_DIR=$HOME/assets
+export DOTFILE_DIR=$ASSET_DIR/dotfiles
