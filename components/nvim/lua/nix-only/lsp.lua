dd0833c393a5a0689e8df657b59fb23b4c9e308e
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-09-14 23:35:16 +0300

    nix only same branch

---
+++ components/nvim/lua/nix-only/lsp.lua
@@ -0,0 +1,32 @@
+-- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/eslint.lua
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+      servers = {
+        --   eslint = {
+        --     filetypes = {
+        --       "javascript",
+        --       "javascriptreact",
+        --       "javascript.jsx",
+        --       "typescript", "typescriptreact",
+        --       "typescript.tsx",
+        --       "vue",
+        --       "svelte",
+        --       "astro",
+        --       "html",
+        --     },
+        --   },
+        --
+        rust_analyzer = {},
+        ruff = {},
+        gopls = {},
+        svelte = {},
+        vtsls = {},
+        yamlls = {},
+        tailwindcss = {}
+      },
+    },
+  },
+}
