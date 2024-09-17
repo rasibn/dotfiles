e1214ed971651cd357d287504e0d564c4b8c3173
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-09-17 12:07:45 +0300

    commit

--- mobile/nvim/lua/plugins/extras.lua
+++ mobile/nvim/lua/plugins/extras.lua
@@ -3,5 +3,6 @@ return {
   { import = "lazyvim.plugins.extras.coding.copilot" },
   { import = "lazyvim.plugins.extras.coding.mini-comment" },
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.editor.mini-files" },
   -- { import = "lazyvim.plugins.extras.editor.harpoon2" },
 }
