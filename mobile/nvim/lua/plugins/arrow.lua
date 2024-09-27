ae9539253295b652c42e9d90df67e62d47f23ec8
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-09-27 15:30:20 +0300

    done with dup, making tmux, nvim one
    done with dup

--- mobile/nvim/lua/plugins/arrow.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "otavioschwanck/arrow.nvim",
-  opts = {
-    show_icons = false,
-    leader_key = "<leader>;",
-    buffer_leader_key = "<leader>m",
-  },
-}
--- mobile/nvim/lua/plugins/extras.lua
+++
@@ -1,8 +0,0 @@
-return {
-  { import = "lazyvim.plugins.extras.coding.yanky" },
-  { import = "lazyvim.plugins.extras.coding.copilot" },
-  { import = "lazyvim.plugins.extras.coding.mini-comment" },
-  { import = "lazyvim.plugins.extras.coding.mini-surround" },
-  { import = "lazyvim.plugins.extras.editor.mini-files" },
-  -- { import = "lazyvim.plugins.extras.editor.harpoon2" },
-}
