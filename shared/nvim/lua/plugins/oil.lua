334d20c9b1bc1144683cf9029dfd959f3100fd09
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-09-30 01:02:00 +0300

    trying out  fzf

--- shared/nvim/lua/plugins/oil.lua
+++ shared/nvim/lua/plugins/oil.lua
@@ -1,18 +1,20 @@
 return {
   "stevearc/oil.nvim",
   lazy = false,
-  dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
-    { "<leader>o",  "<cmd>Oil<CR>", desc = "Oil file Explorer" },
-    -- { "<leader>e",  "<cmd>Oil<CR><C-p>", desc = "Oil file Explorer (Preview)" },
-    { "<leader>fo", "<cmd>Oil<CR>", desc = "Oil file Explorer" },
-    { "<c-l>",      false },
-    { "<C-h>",      false },
+    { "<leader>e", "<cmd>Oil<CR>", desc = "Explorer" },
+    { "<leader>o", "<cmd>Oil<CR>", desc = "Explorer" },
+    { "c-l",       false },
+    { "c-k",       false },
+    { "c-j",       false },
+    { "c-h",       false },
   },
+
   opts = {
-    default_file_explorer = true,
     view_options = {
       show_hidden = true,
     },
   },
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
 }
