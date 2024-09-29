d0a5ba4f178e85a91c8da6ee55ed53edd5ef58d0
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-09-29 23:16:41 +0300

    i think having neotree back is nice

--- components/nvim/lua/plugins/extras.lua
+++ components/nvim/lua/plugins/extras.lua
@@ -2,6 +2,6 @@ return {
   { import = "lazyvim.plugins.extras.coding.yanky" },
   { import = "lazyvim.plugins.extras.coding.mini-comment" },
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
-  { import = "lazyvim.plugins.extras.editor.mini-files" },
+  -- { import = "lazyvim.plugins.extras.editor.mini-files" },
   -- { import = "lazyvim.plugins.extras.coding.copilot" },
 }
