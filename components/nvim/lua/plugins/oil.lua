bf971bca409ecfa22190b84d9829902c54ac50df
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-09-29 23:16:41 +0300

    i think having neotree back is nice

--- components/nvim/lua/plugins/oil.lua
+++ components/nvim/lua/plugins/oil.lua
@@ -3,10 +3,11 @@ return {
   lazy = false,
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
-    { "<leader>fe", "<cmd>Oil<CR>", desc = "Oil file Explorer" },
-    { "<leader>e", "<cmd>Oil<CR><C-p>", desc = "Oil file Explorer (Preview)" },
-    { "<c-l>", false },
-    { "<C-h>", false },
+    { "<leader>o",  "<cmd>Oil<CR>", desc = "Oil file Explorer" },
+    -- { "<leader>e",  "<cmd>Oil<CR><C-p>", desc = "Oil file Explorer (Preview)" },
+    { "<leader>fo", "<cmd>Oil<CR>", desc = "Oil file Explorer" },
+    { "<c-l>",      false },
+    { "<C-h>",      false },
   },
   opts = {
     default_file_explorer = true,
