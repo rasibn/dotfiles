68eb401b2060fb0f2309184ecd3a34d73c3bd944
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-09-30 05:10:33 +0500

    fzf

--- shared/nvim/lua/plugins/extras.lua
+++ shared/nvim/lua/plugins/extras.lua
@@ -2,6 +2,7 @@ return {
   { import = "lazyvim.plugins.extras.coding.yanky" },
   { import = "lazyvim.plugins.extras.coding.mini-comment" },
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.editor.fzf" },
   -- { import = "lazyvim.plugins.extras.editor.mini-files" },
   -- { import = "lazyvim.plugins.extras.coding.copilot" },
 }
