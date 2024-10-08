73a010dfcd7ba499df4beeb128075580cd2f52bb
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-10-08 23:52:01 +0500

    macos specific lsp

---
+++ shared/nvim/lua/pc-only/lsp-macos.lua
@@ -0,0 +1,54 @@
+-- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/eslint.lua
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+      servers = {
+        rust_analyzer = {},
+        ruff = {},
+        gopls = {},
+        yamlls = {},
+        sourcekit = {},
+        tailwindcss = {},
+        volar = {}, -- vue-language-server
+        -- ts_ls = {},
+        -- nil_ls = {},
+        -- ocamllsp = {},
+        -- -- javascript
+        --   eslint = {
+        -- tailwindcss = {},
+        --     filetypes = {
+        --       "javascript",
+        --       "javascriptreact",
+        --       "javascript.jsx",
+        --       "typescript", "typescriptreact",
+        --       "typescript.tsx",
+        --       "vue",
+        --       "svelte",
+        --       "astro",
+        --       "html",
+        --     },
+        --   },
+        -- svelte = {},
+        -- vtsls = {},
+      },
+    },
+  },
+  -- adding formatting here as well because why not
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- Conform will run multiple formatters sequentially
+        python = { "isort", "black" },
+        -- You can customize some of the format options for the filetype (:help conform.format)
+        rust = { "rustfmt", lsp_format = "fallback" },
+        -- Conform will run the first available formatter
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        nix = { "alejandra" },
+      },
+    },
+  },
+}
