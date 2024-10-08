81dffa17adc6dee76b499a0513fcd69310f93f5b
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-10-09 01:37:10 +0500

    oil nvim fixed

--- shared/nvim/lua/plugins/oil.lua
+++ shared/nvim/lua/plugins/oil.lua
@@ -1,36 +1,46 @@
 return {
-  "stevearc/oil.nvim",
-  lazy = false,
-  keys = {
-    { "<leader>e", "<cmd>Oil<CR>", desc = "Explorer" },
-    { "<leader>o", "<cmd>Oil<CR>", desc = "Explorer" },
-  },
-
-  opts = {
-    view_options = {
-      show_hidden = true,
+  {
+    "stevearc/oil.nvim",
+    lazy = false,
+    keys = {
+      { "<leader>e", "<cmd>Oil<CR>", desc = "Explorer" },
+      { "<leader>o", "<cmd>Oil<CR>", desc = "Explorer" },
     },
 
-    keymaps = {
-      ["g?"] = "actions.show_help",
-      ["<CR>"] = "actions.select",
-      ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
-      -- ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
-      ["<C-h>"] = false,
-      ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
-      ["<C-p>"] = "actions.preview",
-      ["<C-c>"] = "actions.close",
-      ["<C-l>"] = false,
-      ["-"] = "actions.parent",
-      ["_"] = "actions.open_cwd",
-      ["`"] = "actions.cd",
-      ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
-      ["gs"] = "actions.change_sort",
-      ["gx"] = "actions.open_external",
-      ["g."] = "actions.toggle_hidden",
-      ["g\\"] = "actions.toggle_trash",
+    opts = {
+      view_options = {
+        show_hidden = true,
+      },
+
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+        -- ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+        ["<C-h>"] = false,
+        ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-l>"] = false,
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      },
+    },
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+
+  -- need this fix because lazyvim lazyloads neo-tree :(
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      { "<leader>e", false },
     },
   },
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
 }
