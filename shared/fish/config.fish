d044def6955c971adaf94b6f1c76a19c9541aa27
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-12-31 18:32:19 +0200

    removed repos

--- shared/fish/config.fish
+++ shared/fish/config.fish
@@ -7,9 +7,6 @@ set -gx EDITOR nvim
 source ~/assets/dotfiles/secrets.sh
 source $DOTFILE_DIR/shared/scripts/.shell.aliases.sh
 
-alias booksm='cd ~/storage/shared/Documents/Books/'
-alias notesm='cd ~/storage/shared/Documents/Vault/'
-
 # Export FZF_DEFAULT_COMMAND
 set -Ux FZF_DEFAULT_COMMAND "fd --type file --follow --hidden --exclude .git"
 
