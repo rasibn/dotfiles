1f293284e68b69ab19de5e0375951a3eb4701dcd
Rasib Nadeem <rasibnadeem101@gmail.com>
2025-03-13 15:15:32 +0500

    yoo

--- shared/nvim/lua/plugins/bufferline.lua
+++ shared/nvim/lua/plugins/bufferline.lua
@@ -1,4 +1,4 @@
 return {
-  { "bufferline.nvim", enabled = false },
+  { "bufferline.nvim", enabled = true },
   --  keys    [b  [B  <S-l>  ]B  ]b  <leader>bo  <leader>bp  <leader>bP  <S-h>  <leader>br  <leader>bl
 }
