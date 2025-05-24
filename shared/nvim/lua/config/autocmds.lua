00dfff32f32debd7dd96fc7ff7bee72214d04d2f
Rasib Nadeem <rasibnadeem101@gmail.com>
2025-05-24 22:27:19 +0500

    disable copilot

--- shared/nvim/lua/config/autocmds.lua
+++ shared/nvim/lua/config/autocmds.lua
@@ -2,7 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- vim.cmd("silent! Copilot disable")
+vim.cmd("silent! Copilot disable")
 vim.cmd("silent! RenderMarkdown toggle")
 --
 -- Add an autocommand for Rust lifetimes `
