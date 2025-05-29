d1e5dbf8ca3aa15c4695f13f1909bf0b83233d10
Rasib Nadeem <rasibnadeem101@gmail.com>
2025-05-30 02:35:14 +0500

    hi

--- nix-hm/home.nix
+++ nix-hm/home.nix
@@ -1,60 +1,97 @@
-{ config, pkgs, ... }:
-
-let 
-	dotfiles_dir = "${config.home.homeDirectory}/assets/dotfiles";
-	assets_dir = "${config.home.homeDirectory}/assets";
-	works_dir = "${config.home.homeDirectory}/work";
-	projects_dir = "${config.home.homeDirectory}/projects";
-in
 {
-	home = {
-		username = "rasib";
-		homeDirectory = "/home/rasib";
-		packages = with pkgs; [
-			wl-clipboard-rs
-			pavucontrol
-			gnumake
+  config,
+  pkgs,
+  ...
+}: let
+  dotfiles_dir = "${config.home.homeDirectory}/assets/dotfiles";
+  assets_dir = "${config.home.homeDirectory}/assets";
+  works_dir = "${config.home.homeDirectory}/work";
+  projects_dir = "${config.home.homeDirectory}/projects";
+in {
+  home = {
+    username = "rasib";
+    homeDirectory = "/home/rasib";
+    packages = with pkgs; [
+      wl-clipboard-rs
+      pavucontrol
+      nil # nix lsp
+      alejandra # nix formator
+      gnumake
       nodejs
-			gcc
-		];
-		sessionVariables = {
-		    EDITOR = "vim";
-		    PROJECT_DIR = projects_dir;
-		    WORK_DIR = works_dir;
-		    ASSET_DIR = assets_dir;
-		    DOTFILE_DIR = dotfiles_dir;
-		  };
-		stateVersion = "24.11";
-		file = {
-			".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/shared/nvim";
-			".tmux.conf".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/shared/.tmux.conf";
-			".wezterm.lua".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/desktop/.wezterm.lua";
-			".config/hypr".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/desktop/linux/hypr";
-		};
-	};
+      gcc
+    ];
+    sessionVariables = {
+      EDITOR = "vim";
+      PROJECT_DIR = projects_dir;
+      WORK_DIR = works_dir;
+      ASSET_DIR = assets_dir;
+      DOTFILE_DIR = dotfiles_dir;
+    };
+    stateVersion = "24.11";
+    file = {
+      # ".tmux.conf".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/shared/.tmux.conf";
+      ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/shared/nvim";
+      ".wezterm.lua".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/desktop/.wezterm.lua";
+      ".config/hypr".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/desktop/linux/hypr";
+    };
+  };
 
-#	wayland.windowManager.hyprland = {
-#		enable = true;
-#		extraConfig = builtins.readFile ./hyprland.conf; 
-#	};
+  #	wayland.windowManager.hyprland = {
+  #		enable = true;
+  #		extraConfig = builtins.readFile ./hyprland.conf;
+  #	};
 
-	  services = {
-	    cliphist = {
-	      enable = true;
-	      allowImages = true;
-	      extraOptions = [ "-max-dedupe-search" "10" "-max-items" "500" ];
-	    }; 
+  services = {
+    cliphist = {
+      enable = true;
+      allowImages = true;
+      extraOptions = ["-max-dedupe-search" "10" "-max-items" "500"];
+    };
+  };
+
+  programs = {
+    wezterm.enable = true;
+    firefox.enable = true;
+    waybar.enable = true;
+    tmux = {
+      enable = true;
+      baseIndex = 1;
+      mouse = true;
+      escapeTime = 0;
+      keyMode = "vi";
+
+      # Remap prefix from 'C-b' to 'C-a'
+      prefix = "C-a";
+
+      plugins = with pkgs.tmuxPlugins; [
+        sensible
+        pain-control
+      ];
+
+      extraConfig = ''
+        set-option -g renumber-windows on
+
+        # Scroll speed configuration
+        set -g @scroll-speed-num-lines-per-scroll 2
+
+
+        # Bind 'v' to copy-mode
+        bind 'v' copy-mode
+
+        # Custom script execution: run-shell "${dotfiles_dir}/shared/scripts/tmux-power.tmux"
+
+        # True colours support
+        set -ga terminal-overrides ",xterm-256color:Tc"
+        set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
+
+        # Underscore colours - needs tmux-3.0
+        set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
+      '';
     };
 
-	programs= {
-		wezterm.enable = true;
-		firefox.enable = true;
-		waybar.enable = true;
-		wofi.enable = true;
-		tmux.enable = true;
-		rofi.enable = true;
-		go.enable = true;
-
+    wofi.enable = true;
+    rofi.enable = true;
+    go.enable = true;
     bun = {
       enable = true;
       enableGitIntegration = true;
@@ -64,7 +101,7 @@ in
     fish = {
       enable = true;
       shellAliases = {
-          vim = "nvim";
+        vim = "nvim";
       };
       shellAbbrs = {
         # system aliases
@@ -89,17 +126,17 @@ in
         set fish_vi_keybindings
       '';
     };
-
     lazygit.enable = true;
-		gh.enable = true;
-		git = {
-			enable = true;
-			userName = "Rasib Nadeem";
-			userEmail = "rasibnadeem101@gmail.com";
-			delta.enable = true;
-			extraConfig = {
-				init.defaultBranch = "main";
-			};
-		};
-	};
+
+    gh.enable = true;
+    git = {
+      enable = true;
+      userName = "Rasib Nadeem";
+      userEmail = "rasibnadeem101@gmail.com";
+      delta.enable = true;
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
+    };
+  };
 }
