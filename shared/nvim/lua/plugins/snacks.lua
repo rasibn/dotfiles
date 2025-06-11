de6876eaed6dbdfe9de94c19d910cb377728db84
Rasib Nadeem <rasibnadeem101@gmail.com>
2025-06-12 02:15:26 +0500

    added picker

---
+++ shared/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/snacks.nvim",
+  ---@module "snacks"
+  ---@type snacks.Config
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          layout = {
+            layout = {
+              position = "right",
+            },
+          },
+        },
+      },
+    },
+  },
+}
