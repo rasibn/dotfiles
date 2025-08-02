a35ef41ab9ed871a5cbcc99b438d974889a96187
Rasib Nadeem <rasibnadeem101@gmail.com>
2025-08-02 22:30:50 +0500

    hi

--- nix-hm/flake.nix
+++ nix-hm/flake.nix
@@ -17,10 +17,35 @@
     system = "x86_64-linux";
     desktopStateVersion = "24.11";
     laptopStateVersion = "25.05";
+    pkgs = nixpkgs.legacyPackages.${system};
+
+    # Custom packages
+    proji = pkgs.buildGoModule {
+      pname = "proji";
+      version = "1.0.0";
+      src = ../shared/scripts/golang/proji;
+      vendorHash = "sha256-2adRLsTSd0vTGcis5FfOT5ZFgB420nvDqHkEEopmgec=";
+      meta = with pkgs.lib; {
+        description = "Tmux session manager with directory selection";
+        license = licenses.mit;
+      };
+    };
+    nvimi = pkgs.buildGoModule {
+      pname = "nvimi";
+      version = "1.0.0";
+      src = ../shared/scripts/golang/nvimi;
+      vendorHash = "sha256-2adRLsTSd0vTGcis5FfOT5ZFgB420nvDqHkEEopmgec=";
+      meta = with pkgs.lib; {
+        description = "fzf based file picker for neovim";
+        license = licenses.mit;
+      };
+    };
   in {
     nixosConfigurations = {
       desktop = nixpkgs.lib.nixosSystem {
-        specialArgs = {stateVersion = desktopStateVersion;};
+        specialArgs = {
+          stateVersion = desktopStateVersion;
+        };
         modules = [
           ./hosts/desktop/hardware.nix
           ./configuration.nix
@@ -28,7 +53,11 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = {stateVersion = desktopStateVersion;};
+            home-manager.extraSpecialArgs = {
+              stateVersion = desktopStateVersion;
+              inherit proji;
+              inherit nvimi;
+            };
             home-manager.users.rasib = {
               imports = [
                 ./home.nix
@@ -40,7 +69,9 @@
       };
 
       laptop = nixpkgs.lib.nixosSystem {
-        specialArgs = {stateVersion = laptopStateVersion;};
+        specialArgs = {
+          stateVersion = laptopStateVersion;
+        };
         modules = [
           ./hosts/laptop/hardware.nix
           ./configuration.nix
@@ -51,7 +82,11 @@
 
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = {stateVersion = laptopStateVersion;};
+            home-manager.extraSpecialArgs = {
+              stateVersion = laptopStateVersion;
+              inherit proji;
+              inherit nvimi;
+            };
             home-manager.users.rasib = {
               imports = [
                 ./home.nix
