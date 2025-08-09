e07cf736ec3d0fd479fab758b93a7ab2722fa0d1
Rasib Nadeem <rasibnadeem101@gmail.com>
2025-08-09 19:38:44 +0500

    feat: adding fzf-lua

--- shared/nvim/lua/plugins/extras.lua
+++ shared/nvim/lua/plugins/extras.lua
@@ -1,5 +1,6 @@
 return {
   { import = "lazyvim.plugins.extras.coding.yanky" },
+  { import = "lazyvim.plugins.extras.editor.fzf" },
   { import = "lazyvim.plugins.extras.coding.mini-comment" },
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
   -- { import = "lazyvim.plugins.extras.editor.fzf" },
---
+++ shared/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,12 @@
+return {
+  "ibhagwan/fzf-lua",
+  opts = function(_, opts)
+    opts.winopts = {
+      fullscreen = true,
+      preview = {
+        vertical = "up:65%",
+        layout = "vertical",
+      },
+    }
+  end,
+}
