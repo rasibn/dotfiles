c8192b480e7685b508248ef8defda8bf91a20757
Rasib Nadeem <rasibnadeem101@gmail.com>
2025-08-11 19:14:08 +0500

    works

--- shared/nvim/lua/plugins/fzf-lua.lua
+++ shared/nvim/lua/plugins/fzf-lua.lua
@@ -1,12 +1,15 @@
 return {
   "ibhagwan/fzf-lua",
-  opts = function(_, opts)
-    opts.winopts = {
-      fullscreen = true,
-      preview = {
-        vertical = "up:65%",
-        layout = "vertical",
-      },
-    }
-  end,
+  -- opts = function(_, opts)
+  --   opts.winopts = {
+  --     fullscreen = true,
+  --     preview = {
+  --       vertical = "up:65%",
+  --       layout = "vertical",
+  --     },
+  --   }
+  -- end,
+  opts = {
+    preview_opts = "hidden",
+  },
 }
