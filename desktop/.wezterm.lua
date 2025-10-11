454ea8179f407c9bc97c8fd893809394b13fb036
Rasib Nadeem <rasibnadeem101@gmail.com>
2025-10-11 20:17:00 +0500

    feat: add a wezterm config that's like tmux

--- desktop/.wezterm.lua
+++ desktop/.wezterm.lua
@@ -1,11 +1,8 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
+-- In newer versions of wezterm, use the config_builder which will help provide clearer error messages
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
@@ -13,35 +10,172 @@ end
 local is_linux = function()
 	return wezterm.target_triple:find("linux") ~= nil
 end
-
 local is_darwin = function()
 	return wezterm.target_triple:find("darwin") ~= nil
 end
-
 local is_windows = function()
 	return wezterm.target_triple:find("windows") ~= nil
 end
 
 -- This is where you actually apply your config choices
-
 -- For example, changing the color scheme:
-config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Catppuccin Mocha"
 config.color_scheme = "Kanagawa (Gogh)"
 
 if is_linux() then
 	config.font_size = 14.0
-	config.enable_tab_bar = false
 end
 
 if is_darwin() then
 	config.font_size = 16
-	config.enable_tab_bar = false
 end
 
 config.window_close_confirmation = "NeverPrompt"
 config.window_background_opacity = 1.00
 config.window_padding = {
 	bottom = 0,
+}
+
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 5000 }
+config.keys = {
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER|SHIFT",
+		key = "|",
+		action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+	},
+	{
+		mods = "LEADER",
+		key = "m",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+	{
+		key = "Enter",
+		mods = "LEADER",
+		action = wezterm.action.ActivateCopyMode,
+	},
+	{
+		mods = "LEADER",
+		key = "h",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		mods = "LEADER",
+		key = "j",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		mods = "LEADER",
+		key = "k",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		mods = "LEADER",
+		key = "l",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		mods = "LEADER",
+		key = "h",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		mods = "LEADER",
+		key = "j",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		mods = "LEADER",
+		key = "k",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		mods = "LEADER",
+		key = "l",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		mods = "LEADER",
+		key = "c",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		mods = "LEADER",
+		key = "q",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	{
+		mods = "CTRL",
+		key = "w",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	{
+		mods = "LEADER",
+		key = "r",
+		action = wezterm.action.ActivateKeyTable({
+			name = "resize_pane",
+			one_shot = false,
+		}),
+	},
+	{
+		key = "[",
+		mods = "LEADER",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		mods = "LEADER",
+		key = "]",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		mods = "LEADER|SHIFT",
+		key = "{",
+		action = wezterm.action.MoveTabRelative(-1),
+	},
+	{
+		mods = "LEADER|SHIFT",
+		key = "}",
+		action = wezterm.action.MoveTabRelative(1),
+	},
+	{
+		mods = "LEADER",
+		key = "1",
+		action = wezterm.action.ActivateTab(0),
+	},
+	{
+		mods = "LEADER",
+		key = "2",
+		action = wezterm.action.ActivateTab(1),
+	},
+	{
+		mods = "LEADER",
+		key = "3",
+		action = wezterm.action.ActivateTab(2),
+	},
+	{
+		mods = "LEADER",
+		key = "4",
+		action = wezterm.action.ActivateTab(3),
+	},
+	{
+		mods = "LEADER",
+		key = "5",
+		action = wezterm.action.ActivateTab(4),
+	},
+}
+config.key_tables = {
+	resize_pane = {
+		{ key = "h", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "j", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "k", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "l", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
+	},
 }
 
 return config
