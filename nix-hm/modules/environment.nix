7f37849a809675652f812158ca1b090f50a5d7d7
Rasib Nadeem <rasibnadeem101@gmail.com>
2026-07-24 04:06:43 +0500

    delete

--- nix-hm/modules/environment.nix
+++ nix-hm/modules/environment.nix
@@ -24,6 +24,7 @@ in {
     ".config/waybar".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/desktop/linux/waybar";
 
     ".config/hypr/hyprland.lua".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/desktop/linux/hypr/hyprland.lua";
+    ".config/hypr/.luarc.json".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/desktop/linux/hypr/.luarc.json";
     ".config/hypr/hyprpaper.conf".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/desktop/linux/hypr/hyprpaper.conf";
     ".config/hypr/monitors.conf".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/desktop/linux/hypr/monitors.conf";
     ".config/hypr/workspaces.conf".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/desktop/linux/hypr/workspaces.conf";
